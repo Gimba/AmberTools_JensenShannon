04e22eea6c067f2aceaa21256345335089775f88
Martin Rosellen <martin.rosellen@fu-berlin.de>
2020-07-21 14:45:36 +0100

    added axis labels

--- AmberJensenCustom.py
+++ AmberJensenCustom.py
@@ -49,7 +49,7 @@ def main(args):
     last_k = 0
     histograms = []
     x_ticks = []
-    for k in range(int(args.frames) - 1, int(traj.n_frames), int(args.frames)):
+    for k in range(int(args.frames), int(traj.n_frames), int(args.frames)):
         angles = pt.dihedral(traj, mask=args.dihedral_angle.replace(',', ' '), top=args.top, frame_indices=range(
             last_k, k + 1))
         hist = list(np.histogram(angles, bins=np.arange(-180, 181, 10))[0])
@@ -63,6 +63,8 @@ def main(args):
 
     plt.scatter(x_ticks[:-1], js_distances)
     plt.xticks(x_ticks[:-1], rotation=90)
+    plt.ylabel('Jensen Shannon Divergence')
+    plt.xlabel('Frame windows')
     plt.tight_layout()
     plt.savefig(top_1_name + ".png")
 
