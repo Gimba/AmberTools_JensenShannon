a2ce4c9891ad0af6a893f7677d5a0d08733c4aa5
Martin Rosellen <martin.rosellen@fu-berlin.de>
2020-08-06 18:10:59 +0100

    bugfix end of frames

--- AmberJensenCustom.py
+++ AmberJensenCustom.py
@@ -49,7 +49,9 @@ def main(args):
     last_k = 0
     histograms = []
     x_ticks = []
-    for k in range(int(args.frames), int(traj.n_frames), int(args.frames)):
+    for k in range(int(args.frames), int(traj.n_frames) + int(args.frames), int(args.frames)):
+        if k >= traj.n_frames:
+            k = traj.n_frames - 1
         angles = pt.dihedral(traj, mask=args.dihedral_angle.replace(',', ' '), top=args.top, frame_indices=range(
             last_k, k + 1))
         hist = list(np.histogram(angles, bins=np.arange(-180, 181, 10))[0])
